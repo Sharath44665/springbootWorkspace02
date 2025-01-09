f6ec100a6052aeb6b91780aa6c87e76b2f809ce5
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-10 00:04:51 +0530

    backend for hello

---
+++ hello/README.md
@@ -0,0 +1,17 @@
+## How to run
+
+from spring boot do this
+
+```
+./mvnw spring-boot:run
+```
+
+from vite-react do this
+
+```
+npm run dev
+```
+
+changes made in vite react files:
+- vite.config.js
+- app.jsx
---
+++ hello/src/test/java/com/example/sharath/SharathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
