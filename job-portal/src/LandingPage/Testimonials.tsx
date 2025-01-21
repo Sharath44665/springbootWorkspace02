619b6fc74c8eebc685bc0c2f133753ac81b82066
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-21 21:18:34 +0530

    add testimonial

---
+++ job-portal/src/LandingPage/Testimonials.tsx
@@ -0,0 +1,49 @@
+import Grid from '@mui/material/Grid2';
+import Container from '@mui/material/Container';
+import { Avatar, Card, CardContent, CardHeader, Rating, Typography } from "@mui/material";
+import { testimonials } from "../Data/Data";
+
+
+function Testimonials() {
+
+    return (
+        <>
+            <div className="px-2 my-5 text-center">
+
+                <div className="text-center fs-1" style={{ textTransform: 'capitalize' }}>
+                    what <span className="text-primary">user</span> say about us?
+                </div>
+
+            </div>
+
+            <Container maxWidth="xl">
+                <Grid container spacing={2}>
+                    {
+                        testimonials.map((item, idx) => {
+                            return (
+                                <Grid>
+                                    <Card sx={{ maxWidth: 350, border: '1px solid lightgrey' }} key={idx}>
+                                        <CardHeader avatar={<Avatar sx={{ backgroundColor: 'blue', height: '50px', width: '50px' }}>S</Avatar>}
+                                            title={<Typography sx={{ fontSize: '22px' }}>{item.name}</Typography>}
+                                            subheader={<Rating name="half-rating" defaultValue={item.rating} precision={0.5} />}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="body2" sx={{ fontSize: '17px' }}>{item.testimonial}</Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            )
+
+                        })
+                    }
+                </Grid>
+            </Container>
+
+
+
+        </>
+    )
+
+}
+
+export default Testimonials;
