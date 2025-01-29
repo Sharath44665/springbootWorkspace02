bbcdf30d64e1863d63e472f3dea6931b1c791594
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-29 17:30:05 +0530

    add job category

--- job-portal-ui/src/Pages/Homepage.tsx
+++ job-portal-ui/src/Pages/Homepage.tsx
@@ -2,14 +2,16 @@ import Header from "../Header/Header";
 import Companies from "../LandingPage/Companies";
 import DreamJob from "../LandingPage/DreamJob";
 import JobCategory from "../LandingPage/JobCategory";
+import Working from "../LandingPage/Workiing";
 
-const HomePage =() => {
+const HomePage = () => {
     return (
         <>
-        <Header/>
-        <DreamJob/>
-        <Companies />
-        <JobCategory />
+            <Header />
+            <DreamJob />
+            <Companies />
+            <JobCategory />
+            <Working />
         </>
     )
 }
