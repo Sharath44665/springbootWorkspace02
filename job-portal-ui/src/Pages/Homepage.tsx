55c4c5aae945841ad766db4a65ae05bf599cf673
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-29 13:51:11 +0530

    add companies marquee

--- job-portal-ui/src/Pages/Homepage.tsx
+++ job-portal-ui/src/Pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import Header from "../Header/Header";
+import Companies from "../LandingPage/Companies";
 import DreamJob from "../LandingPage/DreamJob";
 
 const HomePage =() => {
@@ -6,6 +7,7 @@ const HomePage =() => {
         <>
         <Header/>
         <DreamJob/>
+        <Companies />
         </>
     )
 }
