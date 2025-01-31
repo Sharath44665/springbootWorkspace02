71ac6ad0a5b3574866fb8bcddbbebbdd8347a614
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-01-31 21:19:01 +0530

    add range slider

--- job-portal-ui/src/FindJobs/SearchBar.tsx
+++ job-portal-ui/src/FindJobs/SearchBar.tsx
@@ -1,11 +1,14 @@
 
+import { useState } from "react";
 import { dropdownData } from "../Data/JobsData";
 import MultiInput from "./MutliInput";
+import { RangeSlider } from "@mantine/core";
 
 const SearchBar = () => {
+    const [value, setValue] = useState([1, 10])
     return (
         <>
-            <div className="flex justify-evenly ">
+            <div className="flex justify-evenly gap-2 pt-4 px-2 mx-auto ">
                 {
                     dropdownData.map((dropdownItem, idx) => {
                         return (
@@ -15,6 +18,16 @@ const SearchBar = () => {
                         )
                     })
                 }
+                <div className="w-1/5 ">
+                    <div className="flex justify-between">
+                        <div>Salary</div>
+                        <div>&#8377;{value[0]} LPA - &#8377;{value[1]} LPA</div>
+                    </div>
+
+                    <RangeSlider minRange={1} min={1} max={100} step={1} defaultValue={[1, 10]} onChange={setValue} />
+
+                </div>
+
 
             </div>
 
