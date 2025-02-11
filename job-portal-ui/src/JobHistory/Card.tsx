35e02d36f303172cf7065e466572db1847b896de
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-11 19:37:56 +0530

    add Job history and Applied panel

---
+++ job-portal-ui/src/JobHistory/Card.tsx
@@ -0,0 +1,45 @@
+import { IconBookmark, IconClockHour3 } from "@tabler/icons-react";
+import { Divider, Text } from '@mantine/core';
+import { Link } from "react-router";
+
+const Card = (props:any) => {
+    return (
+
+        <>
+            <Link to='/jobs' className="w-89 border p-2 border-blue-200 rounded-lg hover:border-blue-500 hover:shadow-xl">
+                <div className="flex bg-[#cbedff] text-black justify-between">
+                    <div className="flex gap-2 items-center capitalize ">
+                        <div className="p-1">
+                            <img className="h-7" src={`/Icons/${props.company}.png`} alt="microsoft" />
+                        </div>
+                        <div>
+                            <div className="text-lg">{props.jobTitle}</div>
+                            <div>{props.company} &middot; {props.applicants} applicants</div>
+                        </div>
+                    </div>
+                    <IconBookmark className="cursor-pointer" />
+                </div>
+                <div className="flex capitalize pt-1 text-xs gap-2 [&>div]:py-1 [&>div]:px-2 [&>div]:rounded-lg [&>div]:text-black  [&>div]:bg-[#e1f8ff]">
+                    <div>{props.experience}</div>
+                    <div>{props.jobType}</div>
+                    <div>{props.location}</div>
+                </div>
+                <Text className='text-sm text-justify' lineClamp={3}>
+                    {props.description}
+                </Text>
+                <Divider my="md" />
+                <div className="flex justify-between text-sm">
+                    <div> &#8377; {props.package}</div>
+                    <div className="flex text-gray-400"><IconClockHour3 className="text-sm" /> {props.applied? "Applied": ""} {props.postedDaysAgo} days ago</div>
+                </div>
+
+            </Link>
+
+
+
+
+        </>
+    )
+}
+
+export default Card;
---
+++ job-portal-ui/src/JobHistory/JobHistory.tsx
@@ -0,0 +1,51 @@
+import { Tabs } from "@mantine/core";
+import { jobList } from "../Data/JobsData";
+import Card from "./Card";
+
+const JobHistory = () => {
+    return (<>
+        <div>
+            <div className="text-2xl font-semibold mb-5 ">Job History</div>
+            <Tabs className="[&_button]:!text-xl [&_button[data-active='true']]:!text-blue-600 " variant="outline" radius="md" defaultValue="applied">
+                <Tabs.List>
+                    <Tabs.Tab value="applied">Applied </Tabs.Tab>
+                    <Tabs.Tab value="saved">Saved</Tabs.Tab>
+                    <Tabs.Tab value="offered">Offered</Tabs.Tab>
+                    <Tabs.Tab value="interviewing">Interviewing</Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="applied">
+                    <div className="flex flex-wrap justify-evenly pt-3 gap-4">
+                        {
+                            jobList.map((job, idx) => <Card key={idx} {...job} applied />)
+                        }
+                    </div>
+                </Tabs.Panel>
+                <Tabs.Panel value="saved">
+                    <div className="flex flex-wrap justify-evenly pt-3 gap-4">
+                        {
+                            jobList.map((job, idx) => <Card key={idx} {...job}  />)
+                        }
+                    </div>
+                </Tabs.Panel>
+                <Tabs.Panel value="offered">
+                    <div className="flex flex-wrap justify-evenly pt-3 gap-4">
+                        {
+                            jobList.map((job, idx) => <Card key={idx} {...job} />)
+                        }
+                    </div>
+                </Tabs.Panel>
+                <Tabs.Panel value="interviewing">
+                    <div className="flex flex-wrap justify-evenly pt-3 gap-4">
+                        {
+                            jobList.map((job, idx) => <Card key={idx} {...job} />)
+                        }
+                    </div>
+                </Tabs.Panel>
+
+            </Tabs>
+        </div>
+    </>)
+}
+
+export default JobHistory;
