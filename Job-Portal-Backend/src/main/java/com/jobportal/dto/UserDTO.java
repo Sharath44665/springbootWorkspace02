c7b2dbfdcbd0732d39e563980b4e8f09220692bd
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-13 18:02:10 +0530

    add validation for name, email, password

--- Job-Portal-Backend/src/main/java/com/jobportal/dto/UserDTO.java
+++ Job-Portal-Backend/src/main/java/com/jobportal/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.jobportal.dto;
 
 import com.jobportal.entity.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDTO {
     private String id;
+    @NotBlank(message = "Name is null or blank")
     private String name;
+    @NotBlank(message = "Email is null or blank")
     private String email;
+    @NotBlank(message = "Password is null or blank")
     private String password;
     private AccountType accountType;
 
