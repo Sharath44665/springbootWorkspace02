d44dc82154fe24c2a0612c7755ad1fdb5876f015
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-13 18:20:59 +0530

    add validation rule for password

--- Job-Portal-Backend/src/main/java/com/jobportal/dto/UserDTO.java
+++ Job-Portal-Backend/src/main/java/com/jobportal/dto/UserDTO.java
@@ -1,7 +1,9 @@
 package com.jobportal.dto;
 
 import com.jobportal.entity.User;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,10 @@ public class UserDTO {
     @NotBlank(message = "Name is null or blank")
     private String name;
     @NotBlank(message = "Email is null or blank")
+    @Email(message = "not a valid email")
     private String email;
     @NotBlank(message = "Password is null or blank")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password is not strong or less than 8 charecters")
     private String password;
     private AccountType accountType;
 
