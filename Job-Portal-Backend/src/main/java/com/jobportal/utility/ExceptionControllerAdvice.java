9986c11038aa9d611faf4d6408bb6b5933028662
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-13 17:23:27 +0530

    show error info clearly

---
+++ Job-Portal-Backend/src/main/java/com/jobportal/utility/ExceptionControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.jobportal.utility;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorInfo> generalException(Exception exception){
+        ErrorInfo error = new ErrorInfo(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
