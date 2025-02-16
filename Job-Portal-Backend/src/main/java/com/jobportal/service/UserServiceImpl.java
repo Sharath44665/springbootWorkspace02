f84dc71b1f0fba3c6cb8b35485a806e4b291e3b5
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-16 23:22:52 +0530

    encrypt password: saving password with hash

--- Job-Portal-Backend/src/main/java/com/jobportal/service/UserServiceImpl.java
+++ Job-Portal-Backend/src/main/java/com/jobportal/service/UserServiceImpl.java
@@ -6,16 +6,22 @@ import com.jobportal.exceptions.JobPortalException;
 import com.jobportal.repository.UserRepository;
 import com.jobportal.utility.Utilities;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDTO registerUser(UserDTO userDTO) throws JobPortalException {
         userDTO.setId(Utilities.getNextSequence("users"));
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         User user = userDTO.toEntity();
         user = userRepository.save(user);
         return  user.toDTO();
