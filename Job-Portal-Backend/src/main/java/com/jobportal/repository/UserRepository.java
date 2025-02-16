05332ed54312575e2a2f327e497a2f2dff23a3e5
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-02-16 23:04:55 +0530

    implement id from string to long in mongodb collection

--- Job-Portal-Backend/src/main/java/com/jobportal/repository/UserRepository.java
+++ Job-Portal-Backend/src/main/java/com/jobportal/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.jobportal.repository;
 import com.jobportal.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, Long> {
 
 }
